7b754c2399056bb85811bcb235476adbf8bdd00f
yvedengw <yvedengw@gmail.com>
2021-12-12 19:00:10 +0100

    Init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+__version__ = "0.0.0"
+
+setup(
+    name="cookiecutter-python-example",
+    version=__version__,
+    description="A Cookiecutterr Python Example",
+    author="Leisure Mojo",
+    author_email="leisureMojoTech@gmail.com",
+    license="MIT license",
+    python_requires=">=3.8",
+)
